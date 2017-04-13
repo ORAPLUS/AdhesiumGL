4109653c90ece0ca9a8c8bf3150067ab8a0194f7
Administrateur <Administrateur@ma.adhesium.com>
2017-04-13 16:58:53 +0100

    Gestion des client de notre entreprise Adhesium Technologies

--- AdhesiumGL/src/main/java/org/sid/web/ClientsController.java
+++ AdhesiumGL/src/main/java/org/sid/web/ClientsController.java
@@ -1,8 +1,14 @@
 package org.sid.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ClientsController {
+	
+	@RequestMapping(value ="/pck_gestion.clients/display_clients")
+	public String display_clients(){
+		return "pck_gestion.clients/display_clients";
+	}
 
 }
